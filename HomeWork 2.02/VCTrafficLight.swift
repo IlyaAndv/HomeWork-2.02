3bbb10f242d74b59e985b659284894372037fb5e
Ilya <ktoto47913@gmail.com>
2023-07-23 13:37:03 +0300

    Final settings of view controller objects

--- HomeWork 2.02/VCTrafficLight.swift
+++ HomeWork 2.02/VCTrafficLight.swift
@@ -11,13 +11,20 @@ final class ViewController: UIViewController {
 
     @IBOutlet var trafficLightSwitchButton: UIButton!
     @IBOutlet var trafficLightBaseLabel: UIView!
+    @IBOutlet var redTrafficSignalLabel: UIView!
+    @IBOutlet var yellowTrafficSignalLabel: UIView!
+    @IBOutlet var greenTrafficSignalLabel: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         trafficLightSwitchButton.layer.cornerRadius = 16
         trafficLightBaseLabel.layer.cornerRadius = 16
+        redTrafficSignalLabel.layer.cornerRadius = 47
+        yellowTrafficSignalLabel.layer.cornerRadius = 47
+        greenTrafficSignalLabel.layer.cornerRadius = 47
     }
-
+    
+    
 
 }
 
